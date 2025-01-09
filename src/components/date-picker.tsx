f3aeefc11a1bc11ab759e2dc64033fc7eff56a10
Vitaliy Khatsey <vitaliy.khatsey@gmail.com>
2025-01-09 15:56:41 +0200

    test date picker

--- src/components/date-picker.tsx
+++ src/components/date-picker.tsx
@@ -22,19 +22,15 @@ export default function DatePicker({
 }: DatePickerProps) {
   return (
     <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          size="lg"
-          className={cn(
-            'w-full justify-start text-left font-normal px-3',
+      <PopoverTrigger asChild >
+            <button className={cn(
+            'w-full flex items-center justify-start text-left font-normal px-3 cursor-pointer',
             !value && 'text-muted-foreground',
             className
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 -4" />
-          {value ? format(value, 'PPP') : <span>{placeholder}</span>}
-        </Button>
+          )}>
+            <CalendarIcon className="mr-2 h-4 -4" />
+            {value ? format(value, 'PPP') : <span>{placeholder}</span>}
+            </button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
         <Calendar
@@ -47,3 +43,16 @@ export default function DatePicker({
     </Popover>
   );
 }
+//  <Button
+//           variant="outline"
+//           size="lg"
+//           type='button'
+//           className={cn(
+//             'w-full justify-start text-left font-normal px-3',
+//             !value && 'text-muted-foreground',
+//             className
+//           )}
+//         >
+//           <CalendarIcon className="mr-2 h-4 -4" />
+//           {value ? format(value, 'PPP') : <span>{placeholder}</span>}
+//         </Button> 
