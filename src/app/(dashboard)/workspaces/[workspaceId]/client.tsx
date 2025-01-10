924e8d4af3aec1cb62c924e3d02d5a2782a5a14c
Vitaliy Khatsey <vitaliy.khatsey@gmail.com>
2025-01-10 16:03:36 +0200

    small style fix

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -183,9 +183,9 @@ const MembersList = ({ data, total }: MembersListProps) => {
           {data.map((member) => (
             <li key={member.$id} className="flex-1">
               <Card className="shadow-none border rounded-md overflow-auto">
-                <CardContent className="p-3 flex items-center md:justify-center flex-wrap gap-x-2">
-                  <MemberAvatar name={member.name} className="size-10" />
-                  <div className="flex flex-col items-center min-w-48">
+                <CardContent className="p-3 flex items-center gap-x-2">
+                  <MemberAvatar name={member.name} className="size-14" />
+                  <div className="flex flex-col min-w-48">
                     <p className="text-lg font-medium line-clamp-1">
                       {member.name}
                     </p>
