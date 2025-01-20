e4248129ecb412befe0a648f4c0b0b5559acdbc7
Vitaliy Khatsey <vitaliy.khatsey@gmail.com>
2025-01-20 13:13:23 +0200

    fix wrapper email

--- src/components/email/email-content.tsx
+++ src/components/email/email-content.tsx
@@ -11,7 +11,7 @@ export default function EmailVerification({
   return (
     <EmailWrapper>
       <Text>Hello: <span className='font-bold'>{name}</span>. Copy this link for verification account.</Text>
-      <Link href={href} >Link for verification</Link>
+      <Link href={href}  className='text-xl font-bold'>Verification link</Link>
       <Text>We are glad that you want to join us!</Text>
     </EmailWrapper>
   );
--- src/components/email/email-wrapper.tsx
+++ src/components/email/email-wrapper.tsx
@@ -11,6 +11,7 @@ import {
   Section,
   Text,
   Column,
+  Row,
 } from '@react-email/components';
 interface EmailWrapperProps {
   children: React.ReactNode;
@@ -21,33 +22,42 @@ export default function EmailWrapper({ children }: EmailWrapperProps) {
       <Html>
         <Head />
         <Preview>Task manager</Preview>
-        <Body className="bg-gray-200 font-sans">
-          <Container align='center' className="m-8 mx-auto bg-white rounded overflow-hidden max-w-[600px] h-full" style={{border:`1px solid #dddddd` }}>
-            <Section>
-              <Column>
-                <Img src="https://task-manager-lake-eight.vercel.app/logo.png" />
-              </Column>
-              <Column>
-                <Text className=" font-bold text-blue-400 text-lg">
-                  Task Manager
-                </Text>
-              </Column>
-            </Section>
-            <Section className="px-10">
+        <Body className="bg-gray-200 font-sans py-3">
+          <Container
+            align="center"
+            className="m-8 mx-auto bg-white rounded overflow-hidden max-w-[500px] h-full"
+            style={{ border: `1px solid #dddddd` }}
+          >
+            <Section className="px-10 py-8">
+              <Row>
+                <Column>
+                  <Img
+                    src="https://task-manager-lake-eight.vercel.app/logo.png"
+                    className="w-16 h-12"
+                  />
+                </Column>
+                <Column>
+                  <Text className=" font-bold text-blue-400 text-lg">
+                    Task Manager
+                  </Text>
+                </Column>
+              </Row>
+
               <Hr className="border-gray-300 my-5" />
               {children}
               <Hr className="border-gray-300 my-5" />
-            </Section>
-            <Section className="pb-8">
-              <Section className="px-10">
-                <Text className="leading-6 text-gray-700 ">
-                  Thank you,
-                </Text>
-                <Text className=" leading-6 text-gray-700 ">
-                  The Task Manager team
-                </Text>
-                <Link href="https://task-manager-lake-eight.vercel.app/" className=" font-bold text-blue-400 text-lg"> Task Manager</Link>
-              </Section>
+
+              <Text className="leading-6 text-gray-700 ">Thank you,</Text>
+              <Text className=" leading-6 text-gray-700 ">
+                The Task Manager team
+              </Text>
+              <Link
+                href="https://task-manager-lake-eight.vercel.app/"
+                className=" font-bold text-blue-400 text-lg"
+              >
+                {' '}
+                Task Manager
+              </Link>
             </Section>
           </Container>
         </Body>
